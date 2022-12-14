c3f4ae0521c6b503ea741a21f3de370558310a92
unknown <lemusmauricio@proton.me>
2022-12-14 11:15:19 -0600

    Initial commit

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import data from './data'
+import List from './List'
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState(data)
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}  />
+      <button onClick={() => setPeople([])}>Clear all</button>
+      </section>
+    </main>
+    
   );
 }
 
